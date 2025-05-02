e4f84b962fc6916ec89cec4cac9db452b6c32098
Tilak <tilak999@github.io>
2025-05-02 15:25:08 +0000

    updated

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,31 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+/**
+ * Middleware to check for a valid API key in the request headers
+ * @param request - The incoming request
+ * @returns NextResponse - The response object
+ */
+
+const whitelist = [
+    "/healthcheck",
+]
+
 export function middleware(request: NextRequest) {
-    // Redirect to /admin if accessing the root URL
-    if (request.nextUrl.pathname === "/") {
-        return NextResponse.redirect(new URL("/admin", request.url));
-    } else if (request.nextUrl.pathname === "/admin") {
-        // check if  cf tunnel jwt is valid
+    if (whitelist.some((path) => request.nextUrl.pathname.startsWith(path))) {
         return NextResponse.next();
-    } else {
-        const token = request.headers.get("authorization")
-        if (token) {
-            const raw = Buffer.from(token.split(" ")[1], "base64").toString("utf-8")
-            if (raw !== process.env.API_KEY) {
-                console.error(`Invalid API key: ${token}`)
-                return NextResponse.json({ error: "Invalid API key" }, { status: 401 });
-            }
-            return NextResponse.next();
-        }
-        console.error("API key not found")
-        return NextResponse.json({ error: "API key missing" }, { status: 401 });
     }
+    const token = request.headers.get("authorization")
+    if (token) {
+        const raw = Buffer.from(token.split(" ")[1], "base64").toString("utf-8")
+        if (raw !== "api:" + process.env.API_KEY) {
+            console.error(`Invalid API key: ${token}`)
+            return NextResponse.json({ error: "Invalid API key" }, { status: 401 });
+        }
+        return NextResponse.next();
+    }
+    console.error("API key not found")
+    return NextResponse.json({ error: "API key missing" }, { status: 401 });
 }
 
 // Match all routes
