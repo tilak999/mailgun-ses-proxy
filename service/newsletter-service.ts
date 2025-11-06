7996ae649eea5ae06669817721fbf2135774f4fa
Rafael Calpena Rodrigues <rafael_calpena@hotmail.com>
2025-11-06 14:44:38 -0300

    chore: extract sendSingleMail method

--- service/newsletter-service.ts
+++ service/newsletter-service.ts
@@ -1,6 +1,6 @@
 import { preparePayload } from "../lib/core/aws-utils"
 import { safeStringify } from "../lib/core/common"
-import { SendEmailCommand } from "@aws-sdk/client-sesv2"
+import { SendEmailCommand, SendEmailRequest } from "@aws-sdk/client-sesv2"
 import { DeleteMessageCommand, Message, SendMessageCommand } from "@aws-sdk/client-sqs"
 import { createNewsletterBatchEntry, createNewsletterEntry, createNewsletterErrorEntry, getNewsletterContent } from "./database/db"
 import { QUEUE_URL, sesNewsletterClient, sqsClient } from "./aws/awsHelper"
@@ -32,22 +32,26 @@ export async function addNewsletterToQueue(message: any, siteId: string, auth: a
     return { batchId: message["v:email-id"], messageId: response.MessageId }
 }
 
+async function sendSingleMail(request: SendEmailRequest, dbId: string, siteId: string, batchId: string) {
+    try {
+        const cmd = new SendEmailCommand(request)
+        const resp = await sesNewsletterClient().send(cmd)
+        const messageId = resp.MessageId as string
+        await createNewsletterEntry(messageId, dbId, request)
+        log.info({ messageId, siteId }, "email sent")
+    } catch (e) {
+        log.error(e, "error occurred at sendMail")
+        await createNewsletterErrorEntry(String(e), siteId, batchId, request)
+        return { errorMessage: e }
+    }
+}
+
 async function sendMail(siteId: string, dbId: string) {
     const contents = await getNewsletterContent(dbId)
     const sendEmailRequests = preparePayload(contents, siteId)
     const batchId = contents["v:email-id"]
-    for (const requests of sendEmailRequests) {
-        try {
-            const cmd = new SendEmailCommand(requests)
-            const resp = await sesNewsletterClient().send(cmd)
-            const messageId = resp.MessageId as string
-            await createNewsletterEntry(messageId, dbId, requests)
-            log.info({ messageId, siteId }, "email sent")
-        } catch (e) {
-            log.error(e, "error occurred at sendMail")
-            await createNewsletterErrorEntry(String(e), siteId, batchId, requests)
-            return { errorMessage: e }
-        }
+    for (const request of sendEmailRequests) {
+        await sendSingleMail(request, dbId, siteId, batchId);
     }
     return { batchId }
 }
